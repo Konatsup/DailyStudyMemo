db48ebf9e815e13c8e2fc221d4b95f6f683dc672
Konatsup <konatsukomatsu@gmail.com>
2018-07-03 23:59:13 +0900

    Actionbarにアイコン追加

--- app/src/main/java/com/konatsup/dailystudymemo/MainActivity.java
+++ app/src/main/java/com/konatsup/dailystudymemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.konatsup.dailystudymemo;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import io.realm.Realm;
 
@@ -20,6 +22,25 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText("tab 2"));
         tabLayout.addTab(tabLayout.newTab().setText("tab 3"));
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.filter:
+                // ボタンをタップした際の処理を記述
+                break;
+            case R.id.switchButton:
+                // ボタンをタップした際の処理を記述
+                break;
+
+        }
+        return true;
     }
 
     @Override
