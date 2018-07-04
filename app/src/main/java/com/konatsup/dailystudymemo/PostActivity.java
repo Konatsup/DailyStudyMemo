f85b6000b046c9ae0a3c7302c31b9e46ab8c395f
Konatsup <konatsukomatsu@gmail.com>
2018-07-05 01:22:44 +0900

    投稿画面のおおよそのレイアウトできた

--- app/src/main/java/com/konatsup/dailystudymemo/PostActivity.java
+++ app/src/main/java/com/konatsup/dailystudymemo/PostActivity.java
@@ -1,7 +1,7 @@
 package com.konatsup.dailystudymemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class PostActivity extends AppCompatActivity {
 
